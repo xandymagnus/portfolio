a0230b0f90a88e0c22c16e6884ba1dc1cadb90d7
xandymagnus <xandy.magno325@gmail.com>
2026-08-30 23:32:59 -0300

    add functionality to show/hide back-to-top button on scroll

--- js/main.js
+++ js/main.js
@@ -118,3 +118,37 @@ sections.forEach(section=>{
     sectionObserver.observe(section);
 
 });
+
+/* ============================================
+   BACK TO TOP
+============================================ */
+
+const backToTop =
+    document.querySelector("#back-to-top");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY > 700){
+
+        backToTop.classList.add("show");
+
+    }
+
+    else{
+
+        backToTop.classList.remove("show");
+
+    }
+
+});
+
+backToTop.addEventListener("click",()=>{
+
+    window.scrollTo({
+
+        top:0,
+        behavior:"smooth"
+
+    });
+
+});
